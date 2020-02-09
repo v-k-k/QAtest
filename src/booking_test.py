7cfb2dc6dc96e9fbf39910447e62b73c1e9e8c2f
v-k-k <vladimirkrishtop@gmail.com>
2020-02-10 00:02:50 +0200

    upgrading second scenario

--- src/booking_test.py
+++ src/booking_test.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import StaleElementReferenceException, WebDriverException
 import re
 
 
@@ -41,37 +42,51 @@ class BookingTest:
     def browser(self):
         return self._browser
 
+    @property
+    def wait(self):
+        return WebDriverWait(self.browser, 40)
+
+    @property
+    def mouse(self):
+        return ActionChains(self.browser)
+
     def first_scenario(self, n):
-        browser = self.browser
         children = int(n)
-
-        browser.get(self.url)
-        wait = WebDriverWait(browser, 40)
-        mouse = ActionChains(browser)
-        field = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="xp__guests__toggle"]')))
-        block = browser.find_element_by_id("xp__guests__inputs-container")
-
+        self.browser.get(self.url)
+        field = self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="xp__guests__toggle"]')))
+        block = self.browser.find_element_by_id("xp__guests__inputs-container")
         while not block.is_displayed():
-            mouse.move_to_element(field).click().perform()
+            self.mouse.move_to_element(field).click().perform()
 
-        button = browser.find_element_by_xpath('//form/div[1]/div[3]/div[2]/div/div/div[2]/div/div[2]/button[2]')
+        button = self.browser.find_element_by_xpath('//form/div[1]/div[3]/div[2]/div/div/div[2]/div/div[2]/button[2]')
 
         for i in range(children):
-            browser.implicitly_wait(20)
+            self.browser.implicitly_wait(20)
             button.click()
 
-        browser.implicitly_wait(20)
-        count_age_inputs = len(browser.find_elements_by_xpath('//form/div[1]/div[3]/div[2]/div/div/div[4]/select'))
-        browser.close()
+        self.browser.implicitly_wait(20)
+        count_age_inputs = len(self.browser.find_elements_by_xpath('//form/div[1]/div[3]/div[2]/div/div/div[4]/select'))
+        self.browser.close()
         return count_age_inputs
 
     def second_scenario(self):
-        browser = self.browser
-        browser.get(self.url)
-        browser.implicitly_wait(60)
-        first_city = browser.find_element_by_css_selector("div.promotion-postcard__large:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)")
-        first_city.click()
+        self.browser.get(self.url)
+        self.browser.implicitly_wait(60)
+        self.browser.execute_script("window.scrollTo(0,350)")
+        first_city = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div.promotion-postcard__large:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)")))
+        fail_click = True
+        while fail_click:
+            try:
+                self.mouse.move_to_element(first_city).click().perform()
+            except StaleElementReferenceException:
+                fail_click = False
+            except WebDriverException:
+                fail_click = False
 
-        browser.implicitly_wait(60)
-
-        browser.close()
+        self.browser.implicitly_wait(60)
+        check_in_calendar = self.browser.find_element_by_xpath("//form/div[3]/div/div[1]/div[1]/div/div/div[2]/div[1]")
+        calendar = check_in_calendar.is_displayed()
+        hotel_list = self.browser.find_element_by_id("hotellist_inner")
+        hotels = hotel_list.is_displayed()
+        print(calendar, hotels)
+        self.browser.close()
